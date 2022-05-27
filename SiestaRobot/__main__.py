b151220e09a9df85a8cc9e489fbda923633fea90
Husbando <103307099+Husbandoo@users.noreply.github.com>
2022-05-27 17:42:49 +0530

    Update __main__.py

--- SiestaRobot/__main__.py
+++ SiestaRobot/__main__.py
@@ -410,15 +410,17 @@ def siesta_about_callback(update, context):
                 reply_markup=InlineKeyboardMarkup(
                     [
                         [
-                            InlineKeyboardButton(text=gs(chat.id, "about_button"), callback_data="siesta_"),
+                            InlineKeyboardButton(
+                                text=gs(chat.id, "add_bot_to_group_button"), url="t.me/KaguyaProbot?startgroup=new"),
                         ],
                         [
+                            InlineKeyboardButton(text=gs(chat.id, "aogiri_button"), url="t.me/AogiriNetwork"),
                             InlineKeyboardButton(text=gs(chat.id, "help_button"), callback_data="help_back"),
-                            InlineKeyboardButton(text=gs(chat.id, "darling_button"), url="t.me/Itz_Light_Yagami"),
-                            InlineKeyboardButton(text=gs(chat.id, "chat_grp_button"), url="t.me/night_raiderss"),
+                            InlineKeyboardButton(text=gs(chat.id, "owner_button"), url="t.me/Husbandoo"),
                         ],
                         [
-                            InlineKeyboardButton(text=gs(chat.id, "add_bot_to_group_button"), url="t.me/shikimoriXrobot?startgroup=new"),
+                            InlineKeyboardButton(text=gs(chat.id, "support_button"), url="t.me/ShinomiyaSupport"),
+                            InlineKeyboardButton(text=gs(chat.id, "updates_button"), url="t.me/ShinomiyaUpdates"),
                         ]
                     ]
                 ),
@@ -522,15 +524,17 @@ def Source_about_callback(update, context):
                 reply_markup=InlineKeyboardMarkup(
                     [
                         [
-                            InlineKeyboardButton(text=gs(chat.id, "about_button"), callback_data="siesta_"),
+                           InlineKeyboardButton(
+                                text=gs(chat.id, "add_bot_to_group_button"), url="t.me/KaguyaProbot?startgroup=new"),
                         ],
                         [
+                            InlineKeyboardButton(text=gs(chat.id, "aogiri_button"), url="t.me/AogiriNetwork"),
                             InlineKeyboardButton(text=gs(chat.id, "help_button"), callback_data="help_back"),
-                            InlineKeyboardButton(text=gs(chat.id, "darling_button"), url="t.me/Itz_Light_Yagami"),
-                            InlineKeyboardButton(text=gs(chat.id, "chat_grp_button"), url="t.me/night_raiderss"),
+                            InlineKeyboardButton(text=gs(chat.id, "owner_button"), url="t.me/Husbandoo"),
                         ],
                         [
-                            InlineKeyboardButton(text=gs(chat.id, "add_bot_to_group_button"), url="t.me/shikimoriXrobot?startgroup=new"),
+                            InlineKeyboardButton(text=gs(chat.id, "support_button"), url="t.me/ShinomiyaSupport"),
+                            InlineKeyboardButton(text=gs(chat.id, "updates_button"), url="t.me/ShinomiyaUpdates"),
                         ]
                     ]
                 ),
