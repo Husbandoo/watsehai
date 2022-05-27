c6ba357582a94840fc629d1668ba91cec0ae3e2b
Husbando <103307099+Husbandoo@users.noreply.github.com>
2022-05-27 17:57:28 +0530

    Update logomaker.py

--- SiestaRobot/modules/logomaker.py
+++ SiestaRobot/modules/logomaker.py
@@ -197,7 +197,7 @@ async def logo_(event):
         await tbot.send_file(
             event.chat_id,
             file=flnme,
-            caption="Logo by [Kaguya Shinomiya ](https://t.me/KaguyaProBot)",
+            caption="Logo by [Kaguya Shinomiya](https://t.me/KaguyaProBot)",
             force_document=False,
         )
         os.remove(flnme)
