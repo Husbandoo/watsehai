a8b66a07f7c9e3a38d32e48444b1482a6dadc052
Husbando <103307099+Husbandoo@users.noreply.github.com>
2022-05-28 12:10:01 +0530

    Create script.py

---
+++ SiestaRobot/script.py
@@ -0,0 +1,6 @@
+KAGUYA_IMG = (
+      "https://telegra.ph/file/2023d7c4a27551deaf8fa.jpg",
+      "https://telegra.ph/file/2023d7c4a27551deaf8fa.jpg",
+      "https://telegra.ph/file/2023d7c4a27551deaf8fa.jpg",
+      "https://telegra.ph/file/2023d7c4a27551deaf8fa.jpg",
+)
